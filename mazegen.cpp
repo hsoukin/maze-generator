2a77fc371c32e29ee8961a9acedaa6b7c2b8e6e8
hsoukin <domb.g@libero.it>
2020-07-16 23:38:24 +0200

    Rework inner logic; can now read maze from file

--- mazegen.cpp
+++ mazegen.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <stack>
 #include <random>
 #include <time.h>
@@ -24,9 +26,9 @@ enum Cardinal : uint8_t
 
 // bounds are effectively only used for the outmost cells in the grid
 // v_list contains any neighbours to the current cell which have been visited
-Cardinal pickDirection(const uint8_t &bounds = 0b0000, const uint8_t &v_list = 0b0000)
+Cardinal pickDirection(const uint8_t &v_list = 0b0000)
 {
-	uint8_t mask = ((~bounds) & (~v_list)) & 0x0F;
+	uint8_t mask = (~v_list) & 0x0F;
 	if(mask != 0b0000) do
     {
         uint8_t gen = (rand() % 15) + 1; // random int 1-15
@@ -51,14 +53,12 @@ struct Cell
     uint8_t bounds = Cardinal::NONE;
     bool visited = false;
 
-    Cell() : x(0), y(0), bounds(0) {} // array mumbo jumbo
-
-    Cell(int _x, int _y, uint8_t _bounds = Cardinal::NONE)
-        : x(_x), y(_y), bounds(_bounds) {} // i don't think i ever use this
-
+    Cell() : x(0), y(0), bounds(0x0F) {} // array mumbo jumbo
+    
+    ~Cell() {}
 
     // direction is used to draw over the wall of the cell traveling *to*
-    void DrawSelf(olc::PixelGameEngine* pge, Cardinal direction, olc::Pixel color = olc::GREEN)
+    void DrawSelf(olc::PixelGameEngine* pge, uint8_t direction, olc::Pixel color = olc::GREEN)
     {
         {
             int x = this->x * (3+1); // pixels per square + border gap
@@ -67,26 +67,65 @@ struct Cell
             pge->FillRect(x, y, 3, 3, color);
 
             // now clear the border where moving to
-            switch(direction)
-            {
-                case Cardinal::N:
-                    pge->DrawLine(x, y-1, x+2, y-1, color);
-                    break;
-                case Cardinal::E:
-                    pge->DrawLine(x+3, y, x+3, y+2, color);
-                    break;
-                case Cardinal::W:
-                    pge->DrawLine(x-1, y, x-1, y+2, color);
-                    break;
-                case Cardinal::S:
-                    pge->DrawLine(x, y+3, x+2, y+3, color);
-                    break;
-                default:
-                    break;
-            }
+            if(direction & Cardinal::N)
+                pge->DrawLine(x, y-1, x+2, y-1, color);
+            if(direction & Cardinal::E)
+                pge->DrawLine(x+3, y, x+3, y+2, color);
+            if(direction & Cardinal::W)
+                pge->DrawLine(x-1, y, x-1, y+2, color);
+            if(direction & Cardinal::S)
+                pge->DrawLine(x, y+3, x+2, y+3, color);
         }
     }
+
+    friend std::ostream& operator<< (std::ostream &o, Cell const &e)
+    {
+        o << e.x << ' ' << e.y << ' ';
+        if(e.bounds & Cardinal::N)
+            o << "N ";
+        if(e.bounds & Cardinal::E)
+            o << "E ";
+        if(e.bounds & Cardinal::W)
+            o << "W "; 
+        if(e.bounds & Cardinal::S)
+            o << "S" ;
+        return o;
+    }
+
+    friend std::istream& operator>> (std::istream &i, Cell &e)
+    {
+        std::string line;
+        i >> e.x >> e.y;
+        std::getline(i, line);
+
+        for(auto& ch : line)
+        {
+            if(ch == 'N')
+                e.bounds |= Cardinal::N;
+            else if(ch == 'E')
+                e.bounds |= Cardinal::E;
+            else if(ch == 'W')
+                e.bounds |= Cardinal::W;
+            else if(ch == 'S')
+                e.bounds |= Cardinal::S;
+        }
+
+        return i;
+    }
+
 };
+
+
+inline void log(const Cell *array, int width, int height)
+{
+    int size = width * height;
+    std::ofstream out;
+    out.open("logfile.txt");
+    out << width << ' ' << height << std::endl; 
+    for(int i = 0; i < size; i++)
+        out << array[i] << std::endl;
+    out.close();
+}
 
 
 class MazeGen : public olc::PixelGameEngine
@@ -95,15 +134,28 @@ private:
     const int WIDTH;
     const int HEIGHT;
 
-    // grid = array;
+    bool read;
+
+    // grid is an array;
     Cell *grid;                // i hate matrixes
     std::stack<Cell*> history; // contains a history of visited cells
 
 public:
-    MazeGen(const int &width, const int &height)
-        : WIDTH(width), HEIGHT(height), grid(new Cell[width * height])
+    MazeGen(const int &width, const int &height, Cell *_grid)
+        : WIDTH(width), HEIGHT(height)
     {
         sAppName = "Maze Generator";
+
+        if(_grid)
+        {
+            grid = _grid;
+            read = true;
+        }
+        else
+        {
+            grid = new Cell[width * height] {};
+            read = false;
+        }
     }
 
     bool OnUserCreate() override 
@@ -122,27 +174,26 @@ public:
 
         // i miss python list comprehensions. anyways,
         // this sets the boundaries for the outmost cells of the grid
-        for(int y = 0; y < HEIGHT; y++)
-            for(int x = 0; x < WIDTH; x++)
-            {
-                Cell& cell = grid[y * WIDTH + x];
-                cell.x = x;
-                cell.y = y;
+        if(read)
+        {
+            for(int i = 0; i < WIDTH*HEIGHT; i++)
+                grid[i].DrawSelf(this, ~(grid[i].bounds) & 0x0F);
+        }
+        else
+        {
+            for(int y = 0; y < HEIGHT; y++)
+                   for(int x = 0; x < WIDTH; x++)
+                   {
+                       Cell& cell = grid[y * WIDTH + x];
+                       cell.x = x;
+                       cell.y = y;
+                   }
 
-                if(x == 0)
-                    cell.bounds |= Cardinal::W;
-                else if(x == WIDTH-1)
-                    cell.bounds |= Cardinal::E;
-                if(y == 0)
-                    cell.bounds |= Cardinal::N;
-                else if(y == HEIGHT-1)
-                    cell.bounds |= Cardinal::S;
-            }
-
-        // we then pick a random starting cell.
-        srand(time(NULL));
-        history.push(&grid[ rand() % WIDTH*HEIGHT ]);
-
+               // we then pick a random starting cell.
+               srand(time(NULL));
+               history.push(&grid[ rand() % WIDTH*HEIGHT ]);
+        }
+        
         return true;
     }
     
@@ -162,17 +213,17 @@ public:
 
             // checks all neighbours to determine whether they've been visited before
             uint8_t visited_list = 0b0000;
-            if(y != 0 && grid[pos - WIDTH].visited)
+            if(y == 0 || grid[pos - WIDTH].visited)
                 visited_list |= Cardinal::N;
-            if(x != 0 && grid[pos-1].visited)
+            if(x == 0 || grid[pos-1].visited)
                 visited_list |= Cardinal::W;
-            if(x != WIDTH-1 && grid[pos+1].visited)
+            if(x == WIDTH-1  || grid[pos+1].visited)
                 visited_list |= Cardinal::E;
-            if(y != HEIGHT-1 && grid[pos + WIDTH].visited)
+            if(y == HEIGHT-1 || grid[pos + WIDTH].visited)
                 visited_list |= Cardinal::S;
 
             // out of the pool of available directions to go to, picks one at random (sort of)
-            Cardinal toGo = pickDirection(current.bounds, visited_list);
+            Cardinal toGo = pickDirection(visited_list);
 
             // it's worth noting that after every path calculation, the *previous* location
             // is drawn instead of the new one. I just felt this was more convenient
@@ -180,13 +231,15 @@ public:
             {
                 case Cardinal::NONE:
                     // when you hit a dead end
+                    grid[pos].bounds &= ~(toGo) & 0x0F;
+
                     grid[pos].DrawSelf(this, toGo);
                     history.pop();
                     break;
 
                 case Cardinal::N:
-                    grid[pos].bounds &= ~Cardinal::N;
-                    grid[pos - WIDTH].bounds &= ~Cardinal::S; 
+                    grid[pos].bounds &= ~(toGo) & 0x0F;
+                    grid[pos - WIDTH].bounds &= ~(Cardinal::S) & 0x0F;
 
                     grid[pos].DrawSelf(this, toGo);
 
@@ -194,8 +247,8 @@ public:
                     break;
                 
                 case Cardinal::E:
-                    grid[pos].bounds &= ~Cardinal::E;
-                    grid[pos + 1].bounds &= ~Cardinal::W;
+                    grid[pos].bounds &= ~(toGo) & 0x0F;
+                    grid[pos + 1].bounds &= ~(Cardinal::W) & 0x0F;
 
                     grid[pos].DrawSelf(this, toGo);
 
@@ -203,8 +256,8 @@ public:
                     break;
                 
                 case Cardinal::W:
-                    grid[pos].bounds &= ~Cardinal::W;
-                    grid[pos - 1].bounds &= ~Cardinal::E;
+                    grid[pos].bounds &= ~(toGo) & 0x0F;
+                    grid[pos - 1].bounds &= ~(Cardinal::E) & 0x0F;
 
                     grid[pos].DrawSelf(this, toGo);
 
@@ -212,17 +265,12 @@ public:
                     break;
                 
                 case Cardinal::S:
-                    grid[pos].bounds &= ~Cardinal::S;
-                    grid[pos + WIDTH].bounds &= ~Cardinal::N;
+                    grid[pos].bounds &= ~(toGo) & 0x0F;
+                    grid[pos + WIDTH].bounds &= ~(Cardinal::N) & 0x0F;
 
                     grid[pos].DrawSelf(this, toGo);
 
                     history.push(&grid[pos + WIDTH]);
-                    break;
-                
-                // i remember not being able to compile on code::blocks
-                // without specifying a default case. hm. modern c++ might not need this
-                default:
                     break;
             }
 
@@ -233,6 +281,10 @@ public:
         // quit if the user presses Enter at any point during the process
         if(GetKey(olc::ENTER).bPressed)
             return false;
+        
+        else if(GetKey(olc::S).bPressed)
+            log(grid, WIDTH, HEIGHT);
+
         return true;
     }   
     
@@ -244,22 +296,49 @@ public:
     }
 };
 
-int main()
+int main(int argc, char **argv)
 {
     int width, height;
+    Cell *grid = nullptr;
+    std::ifstream input;
+    bool isRead = false;
 
-    do {
+    if(argc == 3)
+    {
+        std::string str = argv[1];
+        if(str == "-i")
+        {
+            isRead = true;
+            input.open(argv[2]);
+            input >> width >> height;
+            grid = new Cell[width * height] {};
+
+            for(int i = 0; i < width*height; i++)
+            {    
+                grid[i].bounds = 0x00;
+                input >> grid[i];
+            }
+            input.close();
+        }
+        else
+        {
+            width  = std::stoi(argv[1]);
+            height = std::stoi(argv[2]);
+        }
+    }
+    else do {
         std::cout << "Please insert the width and height of the generated maze,\n"
                   << "separated by spaces, then hit Enter: ";
         std::cin  >> width >> height;
 
         if(width <= 0 || height <= 0)
             std::cout << "Please insert valid dimensions for the maze.\n\n";
-    } while(width <= 0 || height <= 0);
+    } while(width <= 1 || height <= 1);
 
-    MazeGen window(width, height); // each one cell is three by three, with a one by three border
+    MazeGen window(width, height, grid);
+
+    // each one cell is three by three, with a one by three border
     if(window.Construct(width*3 + width-1, height*3 + height-1, 3, 3))
         window.Start();
-
     return 0;
 }
